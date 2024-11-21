18a80374b25f220c5e2ed5a43f843130bbb5b3a6
Alex Huaracha <alex.ronny.hq@gmail.com>
2024-11-21 12:48:54 -0500

    Initial Commit

---
+++ examen-danp/examen_danpApp.swift
@@ -0,0 +1,17 @@
+//
+//  examen_danpApp.swift
+//  examen-danp
+//
+//  Created by atipaq on 21/11/24.
+//
+
+import SwiftUI
+
+@main
+struct examen_danpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
